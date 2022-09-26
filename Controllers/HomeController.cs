cb83b6d0bcc02df26cfd731a1283289210291622
Noah Lundquist <nalundquist@gmail.com>
2022-09-26 10:11:19 -0700

    add new variables to base friendletter files
    co-authored-by: Shaniza Lingle <shanizalingle@gmail.com>
    Co-authored-by: Tiberius Lockett <tlockett680@gmail.com>

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using FriendLetter.Models;
+
+namespace FriendLetter.Controllers
+{
+  public class HomeController : Controller
+  {
+		[Route("/hello")]
+    public string Hello() { return "Hello friend!"; }
+
+    [Route("/goodbye")]
+    public string Goodbye() { return "Goodbye friend."; }
+
+		[Route("/")]
+		public ActionResult Letter()
+		{
+			LetterVariable myLetterVariable = new LetterVariable();
+			myLetterVariable.Recipient = "Lina";
+			myLetterVariable.Sender = "Jasmine";
+      myLetterVariable.Location = "Brussels, Belgium";
+      myLetterVariable.Souvenir = "Stroop Waffles";
+			return View(myLetterVariable);
+		}
+
+		
+    [Route("/form")]
+    public ActionResult Form() { return View(); }
+
+		[Route("/postcard")]
+		public ActionResult Postcard(string recipient, string sender, string location, string souvenir)
+		{
+			LetterVariable myLetterVariable = new LetterVariable();
+			myLetterVariable.Recipient = recipient;
+			myLetterVariable.Sender = sender;
+      myLetterVariable.Location = location;
+      myLetterVariable.Souvenir = souvenir;
+			return View(myLetterVariable);
+		}
+  }
+}
